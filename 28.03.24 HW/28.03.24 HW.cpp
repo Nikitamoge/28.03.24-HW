1a50c0af08d85eb63c5fb89c26a1c1df314287a0
Nikita Iliuschchenko <Nike.il.od@gmail.com>
2024-03-29 00:37:52 +0100

    Add project files.

---
+++ 28.03.24 HW/28.03.24 HW.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class Student {
+public:
+    string name;
+    Student(string name) : name(name) {}
+
+    bool operator<(const Student& other) const {
+        return name < other.name;
+    }
+};
+
+int main() {
+    map<Student, int> students;
+    students.insert({ Student("Dima"), 6 });
+    students.insert({ Student("Oleg"), 9 });
+    students.insert({ Student("David"), 10 });
+    students.insert({ Student("Bogdan"), 11 });
+    students.insert({ Student("Sasha"), 12 });
+
+    string searchName = "Oleg";
+    for (const auto& student : students) {
+        if (student.first.name == searchName) {
+            cout << student.first.name << " " << student.second << endl;
+        }
+    }
+}
